7b960918d503bed0d69cf7b5e112a1bd69d70dd6
evan.chiu <bj.evan.chiu@gmail.com>
2023-01-11 15:18:49 +0800

    fixed the callback.

--- lib/acts_as_list/active_record/acts/callback_definer.rb
+++ lib/acts_as_list/active_record/acts/callback_definer.rb
@@ -6,7 +6,7 @@ module ActiveRecord::Acts::List::CallbackDefiner #:nodoc:
       before_validation :check_top_position, unless: :act_as_list_no_update?
 
       before_destroy :reload, unless: Proc.new { new_record? || destroyed_via_scope? || act_as_list_no_update? }
-      after_destroy :decrement_positions_on_lower_items, unless: Proc.new { destroyed_via_scope? || act_as_list_no_update? }
+      after_destroy_commit :decrement_positions_on_lower_items, unless: Proc.new { destroyed_via_scope? || act_as_list_no_update? }
 
       before_update :check_scope, unless: :act_as_list_no_update?
       after_update_commit :update_positions, unless: :act_as_list_no_update?
